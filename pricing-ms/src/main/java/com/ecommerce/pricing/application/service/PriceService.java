6dad94daa3ca8a7c367cba8016d2251d856baf1a
INDRA\icastanof <icastanof@minsait.com>
2025-02-11 11:04:33 +0100

    Add GetProductPriceUseCase implementation and exception.

---
+++ pricing-ms/src/main/java/com/ecommerce/pricing/application/service/PriceService.java
@@ -0,0 +1,32 @@
+package com.ecommerce.pricing.application.service;
+
+import java.time.LocalDateTime;
+import java.util.Comparator;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.ecommerce.pricing.application.dto.PricesResponseDTO;
+import com.ecommerce.pricing.application.port.in.GetProductPriceUseCase;
+import com.ecommerce.pricing.domain.model.Price;
+import com.ecommerce.pricing.domain.repository.PriceRepository;
+import com.ecommerce.pricing.infrastructure.exception.PriceNotFoundException;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class PriceService implements GetProductPriceUseCase {
+    private final PriceRepository priceRepository;
+    
+    @Override
+    public Optional<PricesResponseDTO> getProductPrice(Long brandId, Long productId, LocalDateTime applicationDate) {
+        return priceRepository.findPrices(brandId, productId, applicationDate).stream()
+        .max(Comparator.comparingLong(Price::getPriority))
+        .map(price -> PricesResponseDTO.builder()
+                .productId(price.getProductId()).brandId(price.getBrandId()).priceList(price.getPriceList())
+                .startDate(price.getStartDate()).endDate(price.getEndDate()).finalPrice(price.getFinalPrice())
+                .build())
+        .or(() -> { throw new PriceNotFoundException("No product price found for given criteria"); });
+    }
+}
